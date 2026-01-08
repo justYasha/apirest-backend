0652a3dce2c87fb65975d2cb10c2a9f29c423ae2
justYasha <yasharet.loyola@gmail.com>
2026-01-07 22:19:13 -0600

    Add SQLite connection and GET products endpoint

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port=3000;
+const db= new sqlite3.Database('./database.sqlite');
 
 app.get('/', (req, res) => {
-    res.send('Hello world c:');
+    db.serialize(() => {
+        db.all('SELECT * FROM productos', [], (err, rows) => {
+         if (err) {
+               res.status(500).send(err.message);
+         } else {
+                res.json(rows);
+            }
+        });
+    });
 });
 
 app.listen(port, () => {
