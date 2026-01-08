688f17a6a41fe49c8d28f86f87ddd676af5a5fd5
justYasha <yasharet.loyola@gmail.com>
2026-01-07 23:32:13 -0600

    Add implementation for displaying query results in the console

--- routes/productos.js
+++ routes/productos.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+// Importa la base de datos si es necesario
+const db = require('../db/database');
 router.get('/', (req, res) => {
-    res.send('Lista de productos');
+    //res.send('Lista de productos');
+    db.all('SELECT * FROM productos', [], (err, rows) => {
+        if (err) {
+            res.status(500).send('Error al obtener productos');
+            return;
+        }
+        console.log("Resultados de la consulta:");
+        console.dir(rows, { depth: null });
+        res.json(rows);
+    });
 });
 
 router.get('/:id', (req, res) => {
